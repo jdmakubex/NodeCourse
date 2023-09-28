7bff0a2c179a8491d007449219c6f2fadcd600d1
Juan de Dios <jd_marg@msn.com>
2023-09-28 17:18:09 -0600

    inicia seccion 3: Bases node

---
+++ 03-bases-node/app.js
@@ -0,0 +1,11 @@
+
+
+console.clear();
+
+const base =5;
+console.log('===================');
+console.log(`   Tabla del ${base}`);
+console.log('===================');
+for (let i = 1; i <= 10; i++) {
+    console.log(`${base} x ${i}=  ${base*i}`);
+  }
