e11b1d11a4759df0d2ba96f88ab0d82e91d565ca
Juan de Dios <jd_marg@msn.com>
2023-09-28 17:38:56 -0600

    tabla de multiplicar con archivos

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,18 @@
-
+const { error } = require('console');
+const fs = require('fs');
 
 console.clear();
 
-const base =5;
+const base =6;
 console.log('===================');
 console.log(`   Tabla del ${base}`);
 console.log('===================');
+let salida ='';
 for (let i = 1; i <= 10; i++) {
-    console.log(`${base} x ${i}=  ${base*i}`);
+    salida += `${base} x ${i}=  ${base*i}\n`;
   }
+
+ fs.writeFile(`tabla-${base}.txt`, salida,(err) => {
+    if (err) throw err;
+    console.log(`tabla-${base}.txt creada`);
+ })
