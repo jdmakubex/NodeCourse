614f07918e1d637c6916959e572efaa16b0976c8
Juan de Dios Marcos Gomez <marg.jd@gmail.com>
2023-09-30 20:03:09 -0600

    Implementración de Yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,32 @@
 //const { error } = require('console');
+const yargs = require('yargs');
 const {crearArchivo } = require('./helpers/multiplicar')
+const argv = require('yargs')
+                .option('b',{
+                    alias: 'base',
+                    type: 'number',
+                    demandOption: true
+                })
+                .option('l',{
+                    alias: 'listar',
+                    type: 'boolean',
+                    default: false
+                })
+                .check( (argv,options) => {
+                    if (isNaN(argv.b)){
+                        //console.log('yargs',argv)
+                        throw 'La base tiene que ser un numero'
+                    }
+                    return true
+                })
+                .argv;
 
 console.clear();
-const base =6;
-crearArchivo ( base );
+//const base =6;
+
+//console.log(process.argv)
+console.log(argv)
+
+crearArchivo ( argv.base,argv.l )
+    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+    .catch(err => console.log(err));
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 
-const crearArchivo = (base = 5 ) => {
-    console.log('===================');
-    console.log(`   Tabla del ${base}`);
-    console.log('===================');
-    let salida ='';
-    for (let i = 1; i <= 10; i++) {
-        salida += `${base} x ${i}=  ${base*i}\n`;
+// Con async vierto la función en promesa
+const crearArchivo = async(base,listar=false) => {
+    //metemos todo en un try y catch para el correcto manejo de los errores.
+    try {
+        console.log('===================');
+        console.log(`   Tabla del ${base}`);
+        console.log('===================');
+        let salida ='';
+        for (let i = 1; i <= 10; i++) {
+            salida += `${base} x ${i}=  ${base*i}\n`;
+        }
+        //Si listar viene con true, entonces muestra la salida.
+        listar ? console.log(salida) : null;
+        //Este fragmaneto es para escribir en el archivo el resultado de la salida
+        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        
+        //Retorno de la promesa
+        return `tabla-${base}.txt`;
+
+    }catch(err){
+        throw err;
     }
 
-    console.log(salida);
-    //Este fragmaneto es para escribir en el archivo el resultado de la salida
-    fs.writeFileSync(`tabla-${base}.txt`, salida);
-        console.log(`tabla-${base}.txt creada`);
+
+    
 }
 
 module.exports = {
-    crearArchivo: crearArchivo
+    crearArchivo
 }
