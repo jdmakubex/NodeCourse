4965212870925564c4df52ac1ee8d85dabdda36e
Juan de Dios Marcos Gomez <marg.jd@gmail.com>
2023-10-05 12:49:14 -0600

    mapbox y token

---
+++ 05-clima-app/README.md
@@ -0,0 +1,5 @@
+# Notas
+
+Este proyecto es improtante, porque describe de forma práctica como consumir webservices de la web para poder integrarlos a nuestro proyecto de backend. Utiliza paquetes como: inquirer, colors y axios para hacer peticiones HTTP.
+
+Para que funcione, no olvides ponner en consola un "npm install" para que se instalen las pedendencias.
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -17,7 +17,7 @@ class Busquedas {
              * Axios, es una lbrería que se usa para hacer peticiones HTTP, ya que reques ya se está quedando obsoleta
              * En el siguiente ejemplo hacemos una petición get a un endpoint para solicitarle información
              */
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Oaxaca.json?country=mx&limit=5&language=es&access_token=pk.eyJ1IjoiamRtYWt1YmV4IiwiYSI6ImNsbmRnZXp0OTA0a2gyaXVwa2I2NDMzMTQifQ.pk99Qj6WT9JHBhskPBCNcQ');
             console.log(resp.data);
 
             
