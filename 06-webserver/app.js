bc42e1daee2469d4727b3000bd835ff0e06d8396
Juan de Dios <jd_marg@msn.com>
2023-10-09 17:22:24 -0600

    Introducción a empress

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,2 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 8080;
 
-console.log('hola mundo');
+app.get('/',  (req, res) => {
+  res.send('Home page')
+});
+
+app.get('/hola-mundo',  (req, res) => {
+    res.send('Hi World on the route')
+});
+
+//Uso de comodin
+app.get('*',  (req, res) => {
+    res.send('404 | Page not found')
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listen at http://localhost:${port}`)
+})
