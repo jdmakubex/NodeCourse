4a34cbc5a82479c18f3503c3866cd50b10e110ca
Juan de Dios <jd_marg@msn.com>
2023-10-10 08:56:46 -0600

    Handebars: Argumentos desde el controlador

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 8080;
 
+//TODO: require hbs
+app.set('view engine', 'hbs');
+
 /**
  * Para servir contenido estático se implementan middlewarew's
  */
@@ -10,6 +13,11 @@ app.use( express.static( 'public' ) );
 // app.get('/',  (req, res) => {
 //   res.send('Home page');
 // });
+
+app.get('/',  (req, res) => {
+  res.render('home');
+});
+
 
 app.get('/hola-mundo',  (req, res) => {
     res.send('Hi World on the route');
