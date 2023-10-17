739ccb503254f0577f9717a2a544b190302aa549
Juan de Dios <jd_marg@msn.com>
2023-10-17 12:04:02 -0600

    Imprementación de bcryptjs y express-validator

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,4 +1,8 @@
 const { response, request } = require ('express');
+const bcryptjs = require ('bcryptjs');
+const { validationResult } =  require ('express-validator');
+
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -14,14 +18,36 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPost = (req, res) => {
+const usuariosPost = async(req, res) => {
 
-    const {nombre,edad} = req.body;
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ){
+        return res.status(400).json(errors);
+    }
+
+    const { nombre,correo, password, rol } = req.body;
+    const usuario = new Usuario( { nombre, correo, password, rol } );
+
+    // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({ correo });
+    if ( existeEmail ){
+        return  res.status(400).json({
+            msg: 'Ese correo ya está registrado'
+        })
+    }
+
+
+    //Encriptarla constraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+
+    //
+
+    await usuario.save();
 
     res.json({
         msg: 'post API - controlador',
-        nombre,
-        edad
+        usuario
     });
 }
 
