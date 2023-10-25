2f455b459d0b53f68d0db5970ab105a604fad834
Juan de Dios <jd_marg@msn.com>
2023-10-24 19:39:10 -0600

    Implementación de Json Web tokens

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -6,8 +6,10 @@ class Server {
 
         constructor() {
             this.app = express();
-            this.port = process.env.PORT | 3000;
+            this.port = process.env.PORT;
+
             this.usuariosPath = '/api/usuarios';
+            this.authPath = '/api/auth';
 
             //Conectar a base datos
             this.conectarDB();
@@ -36,6 +38,7 @@ class Server {
         }
 
         routes(){
+            this.app.use(this.authPath, require('../routes/auth'));
             this.app.use(this.usuariosPath, require('../routes/usuarios'));
 
         }
